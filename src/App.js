80c169a6e5a9b884a7bd00b60a1de868340d412a
Manish Kumar Gupta <manishgu231@gmail.com>
2023-03-26 02:31:38 +0530

    Basic functionalities done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
-import Card from "./componenets/Card";
-
-const createDigits = () => {
-	const digits = [];
-	for (let i = 1; i < 10; i++) {
-		digits.push(<button key={i}>{i}</button>);
-	}
-	return digits;
-};
+import { useState } from "react";
 
 function App() {
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const ops = ["+", "-", "/", "*", "."];
+
+	const upadateCalc = (value) => {
+		if (
+			(ops.includes(value) && calc === "") ||
+			(ops.includes(value) && ops.includes(calc.slice(-1)))
+		) {
+			return;
+		}
+
+		setCalc(calc + value);
+
+		if (!ops.includes(value)) {
+			setResult(eval(calc + value).toString());
+		}
+	};
+
+	const createDigits = () => {
+		const digits = [];
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				<button key={i} onClick={() => upadateCalc(i.toString())}>
+					{i}
+				</button>
+			);
+		}
+		return digits;
+	};
+
+	const calculate = () => {
+		setCalc(eval(calc).toString());
+	};
+
+	const del = () => {
+		setCalc(calc.slice(0, -1));
+	};
+
 	return (
 		<div className="App">
 			<div className="calculator">
 				<div className="display">
-					<span>(0) </span> 0
+					{result ? <span>({result})</span> : ""}
+					&nbsp;
+					{calc || "0"}
 				</div>
 				<div className="operators">
-					<button> + </button>
-					<button> - </button>
-					<button> x </button>
-					<button> / </button>
+					<button onClick={() => upadateCalc("+")}> + </button>
+					<button onClick={() => upadateCalc("-")}> - </button>
+					<button onClick={() => upadateCalc("*")}> * </button>
+					<button onClick={() => upadateCalc("/")}> / </button>
 
-					<button>DEL</button>
+					<button onClick={del}>DEL</button>
 				</div>
 				<div className="digits">
 					{createDigits()}
-					<button> 0 </button>
-					<button> . </button>
-					<button> = </button>
+					<button onClick={() => upadateCalc("0")}> 0 </button>
+					<button onClick={() => upadateCalc(".")}> . </button>
+					<button onClick={calculate}> = </button>
 				</div>
 			</div>
 		</div>
