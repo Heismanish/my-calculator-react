2097f118f1619160affdd176dff395799edf3c6d
Manish Kumar Gupta <manishgu231@gmail.com>
2023-03-26 01:30:15 +0530

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import Card from "./componenets/Card";
 
+const createDigits = () => {
+	const digits = [];
+	for (let i = 1; i < 10; i++) {
+		digits.push(<button key={i}>{i}</button>);
+	}
+	return digits;
+};
+
 function App() {
-	return <div className="App">Hello world</div>;
+	return (
+		<div className="App">
+			<div className="calculator">
+				<div className="display">
+					<span>(0) </span> 0
+				</div>
+				<div className="operators">
+					<button> + </button>
+					<button> - </button>
+					<button> x </button>
+					<button> / </button>
+
+					<button>DEL</button>
+				</div>
+				<div className="digits">
+					{createDigits()}
+					<button> 0 </button>
+					<button> . </button>
+					<button> = </button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
